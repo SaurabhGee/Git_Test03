7deead087381ec923c3bd6ed0d4498aedc1a9338
saurabhgoswami03 <saurabhgoswami03@gmail.com>
2022-01-02 19:32:53 +0530

    Committing the code in saurabhgoswami03 user repo.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World2");
-		System.out.println("Hello World3");
-		System.out.println("Hello World4");
+		System.out.println("Testing the Hello World in saurabhgoswami03 user repo.");
 	}
 
 }
